0e1cd296587b584783ec5c51fca15f03ec03e672
Jefry <jefrycruz88@gmail.com>
2015-01-27 03:02:24 -0500

    Added sound effect (commented out for now)

--- app/src/main/java/com/punyweblab/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/punyweblab/funfacts/FunFactsActivity.java
@@ -2,14 +2,12 @@ package com.punyweblab.funfacts;
 
 import android.app.Activity;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Button;
-import android.widget.Toast;
-
 
 public class FunFactsActivity extends Activity {
 
@@ -22,29 +20,21 @@ public class FunFactsActivity extends Activity {
         setContentView(R.layout.activity_fun_facts);
 
         // Declare our view variables and assign variables
+        // final MediaPlayer buttonSound = MediaPlayer.create(this, R.raw.button);
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactButton);
         final RelativeLayout factScreen = (RelativeLayout) findViewById(R.id.screen);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String fact = mFactbook.getFact();
+                //buttonSound.start();
                 factLabel.setText(fact);
                 factScreen.setBackgroundColor(Color.parseColor(mBackgroundColor.getColor()));
             }
         };
 
         showFactButton.setOnClickListener(listener);
-
-        // Lets create a toast and show it
-        String toast_msg = "This is my sample toast.";
-        Toast mToast  = Toast.makeText(this, toast_msg, Toast.LENGTH_LONG);
-        mToast.show();
-
-
-        // Log data
-        Log.d("FunFactsActivity", "This is my custom tag");
     }
-
 }
